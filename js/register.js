1ccd32830a600307e0d14135b6f6caf30c6bc3b2
gowseny <gowsy2@gmail.com>
2022-12-06 20:08:48 +0000

    email

--- js/register.js
+++ js/register.js
@@ -24,6 +24,31 @@ function openLogin() {
 
   }
 
+  function validation() {
+    let form = document.getElementById('form')
+    let email = document.getElementById('Email').value
+    let text = document.getElementById('text')
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+  
+    if (email.match(pattern)) {
+      form.classList.add('valid')
+      form.classList.remove('invalid')
+      text.innerHTML = "Email Address is valid"
+      text.style.color = '#00ff00'
+    } else {
+      form.classList.remove('valid')
+      form.classList.add('invalid')
+      text.innerHTML = "Email Address Not Valid"
+      text.style.color = '#ff0000'
+    }
+  
+    if (email == '') {
+      form.classList.remove('valid')
+      form.classList.remove('invalid')
+      text.innerHTML = ""
+      text.style.color = '#00ff00'
+    }
+  }
 
   //Put register data into a JS object if user clicks 'Register' button
 const registerBtn=document.querySelector("#registerBtn")
