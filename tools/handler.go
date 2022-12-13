6583273c760dd6d3fe6d824f40bced1b1a973edf
AthenaHTA2 <sedmakh2@gmail.com>
2022-12-13 15:47:09 +0000

    Login, cookie, session

--- tools/handler.go
+++ tools/handler.go
@@ -130,58 +130,62 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 	// func CompareHashAndPassword(hashed password, password []byte) error
 	errbcrypt := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	fmt.Println("compare 'passwordhash' with user's pw: ", errbcrypt)
-	fmt.Println("'passwordhash' and 'hash': ", []byte(password), []byte(hash))
+	fmt.Println("Error from comparing 'passwordhash' with user's pw. ('nil' error = success) : ", errbcrypt)
+	fmt.Println("user supplied password and 'passwordhash' from database: \n", string([]byte(password)), string([]byte(hash)))
 	// returns nil on success
+
 	if errbcrypt == nil {
 		stmtCurrentUer := "SELECT * FROM Users WHERE nickName = ?"
 		rowCurrentUser := sqldb.DB.QueryRow(stmtCurrentUer, username)
 
-		err3 := rowCurrentUser.Scan(&CurrentUser.UserID, &CurrentUser.FirstName, &CurrentUser.LastName, &CurrentUser.NickName, &CurrentUser.Age, &CurrentUser.Gender, &CurrentUser.Email, &CurrentUser.Password)
+		var (
+			userID, age                                  int
+			firstName, lastName, nickName, gender, email string
+		)
+		err3 := rowCurrentUser.Scan(&userID, &firstName, &lastName, &nickName, &age, &gender, &email, &password)
 		if err3 != nil {
-			fmt.Println("err3 rowCred.scan:", err3)
+			fmt.Println("error scanning current user's database row:", err3)
 			fmt.Println("error accessing db")
 			return
 		}
-		fmt.Println("All user data from 'Users' table: ", err3)
+		fmt.Println("All current user's data from 'Users' database table: \n", userID, firstName, lastName, nickName, age, gender, email)
 		err3 = IsUserAuthenticated(w, &CurrentUser)
-		fmt.Println("this user logged in: ", err3)
+		//fmt.Println("this user logged in: ", err3)
 		if err3 != nil {
-			//http.WarnMessage(w, "You are already logged in 🧐")
+			fmt.Println("You are already logged in 🧐")
 			fmt.Println("already logged in: ", err3)
 			return
 		}
 		sessionToken := uuid.NewV4().String()
 		expiresAt := time.Now().Add(60 * time.Minute)
-
+		cookieNm := username + "_session"
 		// Finally, we set the client cookie for "session_token" as the session token we just generated
 		// we also set an expiry time of 120 minutes
 		http.SetCookie(w, &http.Cookie{
-			Name:     "session_token",
+			Name:     cookieNm,
 			Value:    sessionToken,
 			MaxAge:   7200,
 			Expires:  expiresAt,
 			HttpOnly: true,
 		})
 		// storing the cookie values in struct to access on other pages.
-		user_session := Cookie{"session_token", sessionToken, expiresAt}
+		user_session := Cookie{cookieNm, sessionToken, expiresAt}
 		fmt.Println("user_session:", user_session)
 		// Duplicates are ignored
 		insertsessStmt, err4 := sqldb.DB.Prepare("INSERT OR IGNORE INTO Sessions (userID, cookieName, cookieValue) VALUES (?, ?, ?);")
-		// fmt.Println("Session Token:", sessionToken)
 		if err4 != nil {
 			fmt.Println("err4 preparing statement:", err4)
 			//WarnMessage(w, "there was a problem logging in")
 			return
 		}
 		defer insertsessStmt.Close()
-		insertsessStmt.Exec(CurrentUser.UserID, "session_token", sessionToken)
+		insertsessStmt.Exec(CurrentUser.UserID, cookieNm, sessionToken)
 
 		// redirect user to index handler after successful login
 		//http.Redirect(w, r, "/", http.StatusFound)
 		//return
 	}
-	fmt.Println("incorrect password")
+	//fmt.Println("incorrect password")
 	//WarnMessage(w, "check username and password")
 
 }
@@ -232,7 +236,7 @@ func InsertSession(u *User, session *http.Cookie) *Session {
 // IsUserAuthenticated ...
 func IsUserAuthenticated(w http.ResponseWriter, u *User) error {
 	var cookieValue string
-	if err := sqldb.DB.QueryRow("SELECT sessionID FROM Sessions WHERE userID = ?", u.UserID).Scan(&cookieValue); err != nil {
+	if err := sqldb.DB.QueryRow("SELECT cookieValue FROM Sessions WHERE userID = ?", u.UserID).Scan(&cookieValue); err != nil {
 		return nil
 	}
 	if err := DeleteSession(w, cookieValue); err != nil {
@@ -266,7 +270,7 @@ func DeleteSession(w http.ResponseWriter, cookieValue string) error {
 	}
 	http.SetCookie(w, cookie)
 
-	stmt, err := sqldb.DB.Prepare("DELETE FROM Sessions WHERE sessionID=?;")
+	stmt, err := sqldb.DB.Prepare("DELETE FROM Sessions WHERE cookieValue=?;")
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
