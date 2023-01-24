f55822286b79fe6ca4d46d0d9b9119bfcc0f1ded
Marsilda <marsildaa93@gmail.com>
2023-01-24 11:23:47 +0000

    deletession 1

--- tools/session.go
+++ tools/session.go
@@ -60,6 +60,7 @@ func IsUserAuthenticated(w http.ResponseWriter, u *User) error {
 	return nil
 }
 
+/*
 // User's cookie expires when browser is closed, delete the cookie from the database.
 func DeleteSession(w http.ResponseWriter, cookieValue string) error {
 	cookie := &http.Cookie{
@@ -76,6 +77,53 @@ func DeleteSession(w http.ResponseWriter, cookieValue string) error {
 	}
 	defer stmt.Close()
 	stmt.Exec(cookieValue)
+	if err != nil {
+		fmt.Println("DeleteSession err: ", err)
+		return err
+	}
+	return nil
+}
+
+*/
+//corrected
+/*
+"DeleteSession" 	function takes in two parameters: a http.ResponseWriter and a string named "cookieValue".
+
+This function is doing the following tasks:
+
+Creating a new cookie with the name of CurrentUser.NickName + "Session_token" and value "",
+setting its MaxAge to -1, and setting HttpOnly to true, which means that the cookie can only
+ be accessed by the server and not by client-side scripts.
+Using the http.SetCookie function to set the cookie on the client's browser.
+Prepare a SQL statement to delete a session record from the 'sessions' table in the database
+with the session ID equals to the cookieValue.
+Executing the prepared statement and handling any errors that may occur.
+Returning nil if there are no errors.
+Here are the few things that can be corrected:
+
+It is better practice to check if the error returned from the Prepare statement and stmt.Exec
+is not nil and return it instead of logging it and continue the execution.
+The function is returning an error type but no error is returned. It is better to return an error
+if there's any occurred.
+The defer statement is useless in this case, because the statement is closed at the end of the
+function, no need to defer it.
+*/
+
+// User's cookie expires when browser is closed, delete the cookie from the database.
+func DeleteSession(w http.ResponseWriter, cookieValue string) error {
+	cookie := &http.Cookie{
+		Name:     CurrentUser.NickName + "Session_token",
+		Value:    "",
+		MaxAge:   -1,
+		HttpOnly: true,
+	}
+	http.SetCookie(w, cookie)
+	//removing session record from 'sessions' table
+	stmt, err := sqldb.DB.Prepare("DELETE FROM Sessions WHERE sessionID = ?;")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(cookieValue)
 	if err != nil {
 		fmt.Println("DeleteSession err: ", err)
 		return err
@@ -100,13 +148,13 @@ func NewUser() *User {
 
 //Find the user by their ID
 func FindByUserID(UID int64) *User {
-	u:= NewUser()
+	u := NewUser()
 
 	if err := sqldb.DB.QueryRow("SELECT userID, firstName, lastNamme, nickName, age, gender, email, passwordhash FROM Users WHERE userID = ?", UID).
-			Scan(&u.UserID, &u.FirstName, &u.LastName, &u.NickName, &u.Age, &u.Gender, &u.Email,
-				 &u.Password); err != nil {
-					fmt.Println("error finding user by ID line 93", err)
-					return nil
-				 }
-				 return u
+		Scan(&u.UserID, &u.FirstName, &u.LastName, &u.NickName, &u.Age, &u.Gender, &u.Email,
+			&u.Password); err != nil {
+		fmt.Println("error finding user by ID line 93", err)
+		return nil
+	}
+	return u
 }
